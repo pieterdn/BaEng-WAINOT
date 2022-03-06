0ac1cad4892c30f8c96bb182dbe724d005693ed5
lucascool12 <71119084+lucascool12@users.noreply.github.com>
2022-03-06 09:30:55 +0100

    betere focus

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 var clickedCards = [];
+var focusedCard;
 
+
+function cardFocus(cardPosition){
+    var card = cardPosition.firstElementChild;
+    if(typeof focusedCard !== 'undefined'){
+        focusedCard.classList.remove("focus");
+    }
+    card.classList.add("focus");
+    focusedCard =  card;
+}
+
+function cardUnfocus(cardPosition){
+    if(typeof focusedCard !== 'undefined'){
+        focusedCard.classList.remove("focus");
+    }
+    focusedCard = undefined;
+}
 
 function clickCard(card){
     if(clickedCards.length > 1){
-        clickedCards.pop().classList.remove("focus");
+        clickedCards.pop().classList.remove("shown");
     }
-    card.classList.add("focus");
+    card.classList.add("shown");
     clickedCards.unshift(card);
 }
