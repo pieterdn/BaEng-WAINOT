3102045a6dbd34786a4df6b26844e4c103d3a3e7
Pieter Dilien <pieterdn2001@hotmail.be>
2022-03-30 18:10:58 +0200

    Added asynchronous file upload

---
+++ uploadHandling.php
@@ -0,0 +1,37 @@
+<?php
+    $currentDir = getcwd();
+    $uploadDirectory = "//uploaded_files/";
+
+    // Store all errors
+    $errors = [];
+
+    // Available file extensions
+    $fileExtensions = ['jpeg','jpg','png','gif'];
+
+    if(!empty($_FILES['fileAjax'] ?? null)) {
+        $fileName = $_FILES['fileAjax']['name'];
+        $fileTmpName  = $_FILES['fileAjax']['tmp_name'];
+        $fileType = $_FILES['fileAjax']['type'];
+        $fileExtension = strtolower(pathinfo($fileName,PATHINFO_EXTENSION));
+
+        $uploadPath = $currentDir . $uploadDirectory . basename($fileName);
+
+        if (isset($fileName)) {
+            if (! in_array($fileExtension,$fileExtensions)) {
+                $errors[] = "JPEG, JPG, PNG and GIF images are only supported";
+            }
+            if (empty($errors)) {
+                $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+                if ($didUpload) {
+                    echo "The image " . basename($fileName) . " has been uploaded.";
+                } else {
+                    echo "An error occurred while uploading. Try again.";
+                }
+            } else {
+                foreach ($errors as $error) {
+                    echo $error . "The following error occured: " . "\n";
+                }
+            }
+        }
+    }
+?>
