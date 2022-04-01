0ac093df9edf31a72ed7b4b6dbd3b7e0cbcbe31b
lucascool12 <71119084+lucascool12@users.noreply.github.com>
2022-04-01 21:22:15 +0200

    tabel server afbeeldingen compleet

--- js/generalGenerator.js
+++ js/generalGenerator.js
@@ -1,5 +1,7 @@
 var selectedImages = [];
-const maxTableHeight = 5;
+var selectedTable = 0;
+const maxServerTableHeight = 5;
+const maxServerTableWidth = 2;
 
 window.addEventListener("load",function(){
     document.spelgenerator.addEventListener("submit",checkInput);
@@ -14,8 +16,8 @@ function addToFileListFromUpload(event){
     let files = document.getElementById('fileAjax').files;
     console.log(files);
     for(let i = 0; i < files.length; i++){
-        selectedImages[selectedImages.length + i] = files[i];
-        addFileToTable(files[i])
+        selectedImages[selectedImages.length + i] = files[i].name;
+        addFileToTable(files[i].name)
     }
 }
 
@@ -26,30 +28,119 @@ function createServerImgTable(){
         return;
     }
     let table = document.createElement("table");
-    for(let i = 0; i < serverImages.length; i++){
-        let td = document.createElement("td");
+    table.setAttribute("id","serverImgTable");
+    let tableAmount = serverImages.length;
+    let arrows = false;
+    if(tableAmount > maxServerTableHeight*maxServerTableWidth){
+        tableAmount = maxServerTableHeight*maxServerTableWidth;
+        arrows = true;
+    }
+    
+
+    if(arrows){
+        let td1 = document.createElement("td");
+        let td2 = document.createElement("td");
+        let tr = document.createElement("tr");
+
+        let arrow_left = document.createElement("i");
+        let arrow_right = document.createElement("i");
+        arrow_left.classList.add("arrow");
+        arrow_left.classList.add("last");
+        arrow_left.classList.add("left");
+        arrow_left.setAttribute("id","prevServerImg");
+        arrow_left.addEventListener("click",previousServerImages);
+        arrow_right.classList.add("arrow");
+        arrow_right.classList.add("right");
+        arrow_right.setAttribute("id","nextServerImg");
+        arrow_right.addEventListener("click",nextServerImages);
+
+        td1.setAttribute("style","text-align:center;width:100px;height:20px");
+        td2.setAttribute("style","text-align:center;width:100px;height:20px");
+        td1.appendChild(arrow_left);
+
+        td2.appendChild(arrow_right);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        table.appendChild(tr);
+    }
+    
+    for(let i = 0; i < tableAmount; i++){
+        let td = document.createElement("td");            
         td.appendChild(document.createTextNode(serverImages[i]));
         td.addEventListener("click",displayServerImage);
         td.setAttribute("style","padding-right:1em");
-        if(i<5){
+        if(i<maxServerTableHeight){
             let tr = document.createElement("tr");
             tr.appendChild(td);
             table.appendChild(tr);
         }else{
-            table.children[i%5].appendChild(td);
+            let appendRow = i%maxServerTableHeight + (arrows ? 1:0);
+            table.children[appendRow].appendChild(td);
+        }
+        
+    }
+    div.appendChild(table);
+}
+
+function nextServerImages(event){
+    let max = serverImages.length/(maxServerTableHeight * maxServerTableWidth);
+    console.log(max);
+    if(selectedTable + 1 > max)
+        return;
+    if(selectedTable == 0)
+        document.getElementById("prevServerImg").classList.remove("last");
+    loadServerImagesFromIndex(++selectedTable);
+    if(selectedTable + 1 > max)
+        document.getElementById("nextServerImg").classList.add("last");
+}
+
+function previousServerImages(event){
+    
+    if(selectedTable == 0)
+        return;
+
+    let max = serverImages.length/(maxServerTableHeight * maxServerTableWidth);
+    if(selectedTable + 1 > max)
+        document.getElementById("nextServerImg").classList.remove("last");
+
+    loadServerImagesFromIndex(--selectedTable);
+    console.log(selectedTable);
+    if(selectedTable == 0)
+        document.getElementById("prevServerImg").classList.add("last");
+}
+
+function loadServerImagesFromIndex(start){
+    start = start*(maxServerTableHeight * maxServerTableWidth);
+    let end = start + maxServerTableHeight * maxServerTableWidth;
+
+    let servtable = document.getElementById("allImages");
+    console.log(servtable);
+    let tds = servtable.getElementsByTagName("td")
+    console.log(start);
+    console.log(end);
+    for(let i = start; i < end; i++){
+        if(i < serverImages.length){
+            if((i - start) < maxServerTableHeight)
+                tds.item(2 + 2*(i%maxServerTableHeight)).innerText = serverImages[i];
+            else
+                tds.item(2 + 2*(i%maxServerTableHeight) + 1).innerText = serverImages[i];
+        }else{
+            if((i - start) < maxServerTableHeight)
+                tds.item(2 + 2*(i%maxServerTableHeight)).innerText = "";
+            else
+                tds.item(2 + 2*(i%maxServerTableHeight) + 1).innerText = "";
         }
     }
-
-    div.appendChild(table);
 }
 
 function displayServerImage(event){
     //console.log(event.target);
+    if(event.target.textContent == "")
+        return;
     let servImg = document.getElementById("serverImage");
     
     let img = document.createElement("img");
     img.onload = function(){
-        
         //console.log(img.naturalWidth + "  " +img.naturalHeight);
         if(img.naturalWidth>img.naturalHeight)
             img.setAttribute("style","width:200px;margin:1em;");
@@ -76,7 +167,7 @@ function addFileToTable(file){
     let tr = document.createElement("tr");
     let td = document.createElement("td");
     tr.appendChild(td);
-    td.appendChild(document.createTextNode(file.name));
+    td.appendChild(document.createTextNode(file));
     table.appendChild(tr);
 }
 
