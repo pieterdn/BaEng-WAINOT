d9d21bf87a77606d0d9958fbee1e17e27791fc5a
JarneVk <jarne.vankerckhove24@gmail.com>
2022-04-28 20:27:57 +0200

    vennekes python changes

---
+++ py/generator.php
@@ -0,0 +1,10 @@
+<?php
+   $PYTHON='python3';
+   putenv("REQUEST_METHOD=GET");
+   $s = http_build_query($_POST);
+   echo "$s";
+   putenv("QUERY_STRING=$s");
+   $command_exec = escapeshellcmd("$PYTHON generator.py");
+   $str_output = shell_exec($command_exec);
+   echo $str_output;
+?>
